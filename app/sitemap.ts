77b960633e558a50d81db569060eaf365253d1e9
prakharbhatia <prakharb88@gmail.com>
2025-11-24 06:49:31 +0530

    Make sitemap function async for proper XML generation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next';
 import { getAllProducts } from '@/lib/products';
 import { getAllBlogPosts } from '@/lib/blog';
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://prakharpsychologicaltest.com';
   const products = getAllProducts();
   const blogPosts = getAllBlogPosts();
